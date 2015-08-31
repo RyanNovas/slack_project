9e75a1ba3877bf93bf89a0421bff4aa33e970ef2
RyanNovas <ryan@quill.org>
2015-08-31 18:24:05 -0400

    prooves that we can set it up

--- slack_with_rails/app/controllers/welcome_controller.rb
+++ slack_with_rails/app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    require 'nokogiri'
+    require 'open-uri'
+    @scrape = Nokogiri::HTML(open("http://guides.rubyonrails.org/active_record_basics.html"))
   end
 end
