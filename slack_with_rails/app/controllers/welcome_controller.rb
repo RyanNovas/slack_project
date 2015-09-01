9a87caeb03b33fff90d88c69f81b52ed54d31364
RyanNovas <ryan@quill.org>
2015-09-01 19:42:33 -0400

    cleans folders

--- slack_with_rails/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def index
-    @view = https://peaceful-crag-3314.herokuapp.com/
-    # @view = Nokogiri::HTML(open("http://guides.rubyonrails.org/active_record_basics.html"))
-  end
-end
-
-@view
