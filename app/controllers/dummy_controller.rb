6046e4457c68f24071c96202695611b80cb76357
RyanNovas <ryan@quill.org>
2015-09-04 13:32:14 -0400

    cleaned up extraneous JSON info

--- app/controllers/dummy_controller.rb
+++ app/controllers/dummy_controller.rb
@@ -12,8 +12,8 @@ require 'rest-client'
 
 
   def test
-    RestClient.post "https://hooks.slack.com/services/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", payload = {"text" => "test" }.to_json, :content_type => :json, :accept => :json
-    #payload = 'payload= > {"channel" => "#general", "username" => "webhookbot", "text" => "This is posted to #general and comes from a bot named webhookbot.", "icon_emoji" => ":ghost:"}'.to_json
-    #Net::HTTP.post_form('https://hooks.slack.com/services/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'), payload)
+    RestClient.post "https://hooks.slack.com/services/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", payload = {"text" => "test" }.to_json#, :content_type => :json, :accept => :json
+
+
   end
 end
